0dbcd862f166329a19867678e642789f711428aa
Yaroslav Kibysh <yaroslavki@interlink-ua.com>
2021-09-12 12:10:20 +0300

    Fix cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const PORT = process.env.port || 2000;
 const app = express();
-
 const contactsController = require("./controllers/contacts-controller");
 
 app.use(express.static(__dirname + "/public"));
+
+app.use(cors());
 
 app.use("/api/contacts", contactsController);
 
