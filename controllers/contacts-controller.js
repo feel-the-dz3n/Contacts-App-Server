64e5f2a583200c8060b3685ca233a3397f32f863
Yaroslav Kibysh <yaroslavki@interlink-ua.com>
2021-09-12 18:39:53 +0300

    Add removing API

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -16,6 +16,23 @@ router.post("/:id", (req, res) => {});
 
 router.put("/:id", (req, res) => {});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  let data = fs.readFileSync(filePath, "utf8");
+  let contacts = JSON.parse(data);
+  let contactIndex = contacts.map((c) => c.id).indexOf(id);
+  console.log({ id: id, map: contacts.map((c) => c.id) });
+
+  if (contactIndex == -1) {
+    res.status(404).send();
+  } else {
+    const removedContact = contacts.splice(contactIndex, 1)[0];
+
+    data = JSON.stringify(contacts);
+    fs.writeFileSync(filePath, data);
+
+    res.send(removedContact);
+  }
+});
 
 module.exports = router;
